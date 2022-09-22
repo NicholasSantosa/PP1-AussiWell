0812e2fd5ddb30dd7dbf6400dd01eacd3e81e991
NicholasSantosa <s3844591@student.rmit.edu.au>
2022-09-22 23:06:20 +1000

    Added Database
    Added Controllers, Requests and updated db connection to AWS credentials

--- website/routes/web.php
+++ website/routes/web.php
@@ -13,6 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::inertia('/', 'Home/Page');
-Route::inertia('/login', 'Auth/Login');
-Route::inertia('/sign-up', 'Auth/Signup');
+// Route::inertia('/', 'Home/Page');
+// Route::inertia('/login', 'Auth/Login');
+// Route::inertia('/sign-up', 'Auth/Signup');
+
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{   
+    /**
+     * Home Routes
+     */
+    Route::get('/', 'HomeController@index')->name('home.index');
+
+    Route::group(['middleware' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+    });
+});
