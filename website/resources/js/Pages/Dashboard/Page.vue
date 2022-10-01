4603075cf5d5e914419bb6fd7dbc7add6a8b3528
Manav <manav.sg1@gmail.com>
2022-10-01 15:43:25 +0530

    ⚡website reads extension products ⚡

--- website/resources/js/Pages/Dashboard/Page.vue
+++ website/resources/js/Pages/Dashboard/Page.vue
@@ -1,18 +1,32 @@
 <template>
 	<div class="prose daisy-prose w-full max-w-xl m-auto">
-		<h1>Dashboard</h1>
 
-		<a href="/logout" class="daisy-btn">Logout</a>
+		<div class="relative">
+			<Transition name="router-view" mode="out-in">
+				<div v-if="productList == null">
+					<h4><i class="text-primary fa fa-cog fa-spin"></i>&nbsp;&nbsp;Loading your cart</h4>
+				</div>
+				<div v-else>
+					<h4>Products in your cart</h4>
+					<div v-for="product in productList">{{ product }}</div>
+				</div>
+			</Transition>
+		</div>
+		<a href="/logout" class="daisy-btn mt-10">Logout</a>
 	</div>
 </template>
 
 <script setup>
 
-import {onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
+
+let productList = ref(null);
 
 onMounted(() => {
 	chrome.runtime.sendMessage("faikcccoaifenipeoglkklceopfnnhoj", 'getLocalStorage', (response) => {
-		console.log(response);
+		setTimeout(() => {
+			productList.value = response;
+		}, 2500)
 	});
 })
 
